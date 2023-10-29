203270f52b96310acd00c583f84cdde29bda1622
Jan Kröger <jan.m.kroeger@gmail.com>
2023-10-29 02:44:31 +0200

    visualization update

--- Code/Datenauswertung/util/Visualization.py
+++ Code/Datenauswertung/util/Visualization.py
@@ -7,7 +7,20 @@ class Visualization:
         self.plot()
         
     def set_color(self):
-        pass
+        COLOR_TEMPLATE = [
+            "#D32F2F",  # Rot
+            "#1976D2",  # Blau
+            "#388E3C",  # Grün
+            "#FBC02D",  # Gelb
+            "#8E24AA",  # Lila
+            "#D84315",  # Orange
+            "#0288D1",  # Himmelblau
+            "#7B1FA2",  # Dunkellila
+            "#C2185B",  # Pink
+            "#7E57C2",  # Indigo
+            "#0288D1",  # Hellblau
+            "#5D4037"   # Braun
+        ]
         
     def plot(self):
         for simulation in self.simulations:
